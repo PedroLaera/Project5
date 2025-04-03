5ce01de60341bf024a39f1ba091494cfe183de1c
Vistorgiese <Vitorgiese@hotmail.com>
2025-04-03 20:58:37 -0300

    Update user.test.ts

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -24,11 +24,11 @@ jest.mock("../src/middleware/authMiddleware", () => {
 
 describe("User Endpoint", () => {
   beforeAll(async () => {
-    console.log("🔁 Sincronizando banco de testes...");
+    console.log("Ligando banco de dados...");
     await sequelize.query("SET FOREIGN_KEY_CHECKS = 0");
     await sequelize.sync({ force: true });
     await sequelize.query("SET FOREIGN_KEY_CHECKS = 1");
-    console.log("✅ Banco sincronizado!");
+    console.log("Banco ligou!");
   });
 
   afterAll(async () => {
@@ -44,49 +44,43 @@ describe("User Endpoint", () => {
       CPF: "538.395.170-95",
       password: "1234568@",
     });
-    console.log(response.body);
     expect(response.status).toBe(201);
   });
 
-  
   //GET BY ID
   test("Get /users shold return a user by ID and return success", async () => {
     const response = await request(app)
-    .get("/users/1")
-    .set({ Authorization: "Auth" });
-    
+      .get("/users/1")
+      .set({ Authorization: "Auth" });
+
     expect(response.status).toBe(200);
   });
-  
+
   describe("User Authentication and Update", () => {
     let token: string;
-  
+
     // Primeiro, fazer login para obter o token
     test("POST /login should authenticate a user and return a token", async () => {
-      const response = await request(app)
-        .post("/login")
-        .send({
-          email: "usuarioTeste@gmail.com",
-          password: "1234568@",
-        });
-  
+      const response = await request(app).post("/login").send({
+        email: "usuarioTeste@gmail.com",
+        password: "1234568@",
+      });
+
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty("token");
-  
+
       token = response.body.token;
-      console.log(token);
     });
-  
+
     test("PUT /users/:id should update the user's data", async () => {
       const response = await request(app)
-        .put("/users/1") 
+        .put("/users/1")
         .send({
           name: "usuario teste EDITADO",
           CPF: "231.339.070-50",
           password: "ReiDelas123EDIT@",
         })
-        .set("Authorization", `Bearer ${token}`); 
-        console.log(token)
+        .set("Authorization", `Bearer ${token}`);
       expect(response.status).toBe(200);
       //expect(response.body).toHaveProperty("name", "usuario teste EDITADO");
     });
