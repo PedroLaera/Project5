9cc00da7915d7109cac51c3adc4b383c6e618899
Pedro Henrique Laera Ferreira de Castro <161781371+PedroLaera@users.noreply.github.com>
2025-04-10 06:05:06 -0300

    Limpeza de Código

--- Atlantis/src/components/custom/CardProfile.tsx
+++ Atlantis/src/components/custom/CardProfile.tsx
@@ -170,18 +170,7 @@ export default function ProfileCard() {
               placeholder="CPF"
             />
           </>
-        ) : (
-          <>
-            <p>
-              <strong>Nome:</strong> {userData.name}
-            </p>
-            <p>
-              <strong>Email:</strong> {userData.email}
-            </p>
-            <p>
-              <strong>CPF:</strong> {userData.CPF}
-            </p>
-          </>
+        
         )}
 
         {/* Botões lado a lado */}
@@ -203,9 +192,6 @@ export default function ProfileCard() {
           </Button>
         </div>
 
-        {successMessage && (
-          <p className="text-green-600 text-center mt-4">{successMessage}</p>
-        )}
       </CardContent>
     </Card>
   );
